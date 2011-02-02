5755721584e554454ba4ad60be50587ba369a01b
Braxton Huggins <braxton@delorum.com>
2011-02-02 14:37:19 -0700

    changing the mysql host

--- wp-config.php
+++ wp-config.php
@@ -25,7 +25,7 @@ define('DB_USER', 'terra');
 define('DB_PASSWORD', 't75hrDmu');
 
 /** MySQL hostname */
-define('DB_HOST', '/tmp/mysql/herminia.sock');
+define('DB_HOST', 'localhost:/tmp/mysql/herminia.sock');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
