c799b63177a0567827c36b96b35d31abe7264274
lyondhill <lyondhill@gmail.com>
2011-02-18 09:14:32 -0700

    crankshaft

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'lyon');
+define('DB_NAME', 'tacotom');
 
 /** MySQL database username */
 define('DB_USER', 'iesha');
