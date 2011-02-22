b22f6923b2cf269c0eab3acb08b93f9c1335dc35
lyondhill <lyondhill@gmail.com>
2011-02-22 16:02:13 -0700

    box

--- wp-config.php
+++ wp-config.php
@@ -34,7 +34,7 @@ define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
 /**#@+
- * Authentication Unique Keys and Salts.
+ * Authenticsstion Unique Keys and Salts.
  *
  * Change these to different unique phrases!
  * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
