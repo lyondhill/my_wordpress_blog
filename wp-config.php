b9f9c2b880660e044dbbb5dc4d78510d364ed894
Lyon <lyon@delorum.com>
2011-03-15 15:21:36 -0600

    push the old databvse changes

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQLPAYRTY TIME settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'nickie'); // lyon
+define('DB_NAME', 'shavon'); // lyon
 
 /** MySQL database username */
-define('DB_USER', 'lesli'); //lolita
+define('DB_USER', 'michelle'); //lolita
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Whpw5Abu'); //LD3CtZOR
+define('DB_PASSWORD', 'QsJAROPo'); //LD3CtZOR
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost:/tmp/mysql/nickie.sock'); //micaela
+define('DB_HOST', 'localhost:/tmp/mysql/shavon.sock'); //micaela
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
