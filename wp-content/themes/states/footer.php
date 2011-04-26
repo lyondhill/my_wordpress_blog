fd78d6012022278a65e188995839f39fcf942042
Lyon <lyon@delorum.com>
2011-04-25 20:07:45 -0600

    FIX: remove my stuff

--- wp-content/themes/states/footer.php
+++ wp-content/themes/states/footer.php
@@ -23,9 +23,6 @@
 						<input type="hidden" name="no_note" value="0">
 						<input type="hidden" name="currency_code" value="USD">
 						<input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_LG.gif:NonHostedGuest">
-						<input type="image" src="https://www.paypalobjects.com/WEBSCR-640-20110401-1/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-						<img alt="" border="0" src="https://www.paypalobjects.com/WEBSCR-640-20110401-1/zh_XC/i/scr/pixel.gif" width="1" height="1">
-						</form>
 					</p>
 					
 	        	</div>
